166d5cae8ce277426412b4f3f37c4ac59d64dca1
Stijn Verhagen <stijn@moonlysoftware.com>
2022-03-14 14:10:00 +0100

    Revert "Added SVG import compatibility"
    This reverts commit d1e7ffd93bf540ffadeb2eb84fa8741a296ea30d.

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,15 @@
-const {
-    getDefaultConfig
-} = require('metro-config');
+const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
     const {
-        resolver: {
-            sourceExts,
-            assetExts
-        },
+        resolver: { sourceExts },
     } = await getDefaultConfig();
     return {
         transformer: {
-            babelTransformerPath: require.resolve(
-                'react-native-sass-transformer'),
-            babelTransformerPath: require.resolve(
-                'react-native-svg-transformer'),
+            babelTransformerPath: require.resolve('react-native-sass-transformer'),
         },
         resolver: {
-            assetExts: assetExts.filter(ext => ext !== 'svg'),
-            sourceExts: [...sourceExts, 'scss', 'sass', 'svg'],
+            sourceExts: [...sourceExts, 'scss', 'sass'],
         },
     };
 })();
