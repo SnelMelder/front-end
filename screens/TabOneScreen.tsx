c87f3136ad710b817743182cb34db837b27a0f6d
Jordy Arntz <hello@jordyarntz.com>
2022-03-14 11:24:12 +0100

    Refactor object styles to Scss

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,25 +1,8 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import styles from './shared.scss';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: '80%',
-    },
-});
 
 const TabOneScreen = () => {
     return (
