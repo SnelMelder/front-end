f83e72a22152543500c87af662cc1eefb8a3be10
Jordy Arntz <hello@jordyarntz.com>
2022-03-14 13:06:38 +0100

    Refactor Header components with Scss

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,52 +1,6 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
-
-const styles = StyleSheet.create({
-    header: {
-        height: 130,
-        alignSelf: 'stretch',
-        padding: 0,
-        backgroundColor: '#555',
-        top: 0,
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        justifyContent: 'space-between',
-    },
-    text: {
-        color: '#000',
-        fontSize: 23,
-        textAlign: 'center',
-    },
-    container: {
-        alignSelf: 'flex-end',
-        backgroundColor: '#FFF',
-        padding: 10,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        bottom: 0,
-    },
-    logo: {
-        width: 70,
-        height: 70,
-        alignSelf: 'center',
-    },
-    button: {
-        backgroundColor: '#fff',
-        width: 70,
-        height: 50,
-        alignSelf: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    buttonLeft: {
-        borderTopRightRadius: 10,
-        borderBottomRightRadius: 10,
-    },
-    buttonRight: {
-        borderTopLeftRadius: 10,
-        borderBottomLeftRadius: 10,
-    },
-});
+import { View, Text, Image, Pressable } from 'react-native';
+import styles from './Header.scss';
 
 const Header = () => {
     return (
