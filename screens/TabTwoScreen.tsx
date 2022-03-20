3d45ca0630d016f98830b0d21b05b1021490fec2
Tara Willink <tarawillink@gmail.com>
2022-03-20 20:52:42 +0100

    Added send melding confirmation popup

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './shared.scss';
 
 import EditScreenInfo from '../components/EditScreenInfo/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { Pressable, Button } from 'react-native';
+import ConfirmationModal from '../components/ConfirmationModal/ConfirmationModal'
 
 const TabTwoScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Tab Two</Text>
-            <View
-                style={styles.separator}
-                lightColor="#eee"
-                darkColor="rgba(255,255,255,0.1)"
-            />
-            <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-        </View>
-    );
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>Tab Two</Text>
+
+          {/* For the confirmation button (Conformation popup) */}
+          <View style={styles.container}>
+            <ConfirmationModal visible={modalVisible}>
+              <Pressable
+                style={styles.button}
+                onPress={() => setModalVisible(false)}
+              >
+                <Text style={[styles.modalText, {fontSize: 20}]}>Ok!</Text> 
+              </Pressable>
+            </ConfirmationModal>
+            <Button onPress={() => setModalVisible(true)} title="Melding versturen"/>
+          </View>
+
+        <View
+          style={styles.separator}
+          lightColor="#eee"
+          darkColor="rgba(255,255,255,0.1)"
+        />
+        <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      </View>
+    </>
+  );
 };
 
 export default TabTwoScreen;
