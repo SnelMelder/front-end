1b6a19ec39fb636704f9815458ed39c3c22f9cca
Stijn Verhagen <stijn@moonlysoftware.com>
2022-03-28 10:36:57 +0200

    Initial commit

---
+++ components/DateTime/DateTime.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Image, Pressable } from 'react-native';
+import styles from './DateTime.scss';
+
+const DateTime = () => {
+    return =(
+    <View style={styles.header}>
+
+    </View>
+    );
+};
+
+export default DateTime;
