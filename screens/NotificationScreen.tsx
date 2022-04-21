8fbf8e4bb8ad9f7c7fbb97f2657c251a248a2774
Tara Willink <tarawillink@gmail.com>
2022-04-21 15:35:56 +0200

    Added Notification Screen

---
+++ screens/NotificationScreen.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Image } from 'react-native';
+import styles from './shared.scss';
+
+import { Text, View } from '../components/Themed';
+import Notifications from '../components/Notifications/Notifications';
+
+const NotificationScreen = () => {
+    return (
+        <>
+            <Image
+                style={styles.logo}
+                source={require('../assets/images/SnelMelder_Home_Logo.png')}
+            />
+
+            <View style={styles.container}>  
+                <Notifications>
+                </Notifications>
+                <View
+                    style={styles.separator}
+                    lightColor="#eee"
+                    darkColor="rgba(255,255,255,0.1)"
+                />
+                
+            </View>
+        </>
+    );
+};
+
+export default NotificationScreen;
