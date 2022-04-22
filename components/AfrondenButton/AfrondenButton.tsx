08eb948d9f89bb496680a8fe7a723b40e68b0113
Jordy Arntz <hello@jordyarntz.com>
2022-04-22 14:10:50 +0200

    Prettier fixes

--- components/AfrondenButton/AfrondenButton.tsx
+++ components/AfrondenButton/AfrondenButton.tsx
@@ -3,13 +3,13 @@ import { Text, View } from 'react-native';
 import styles from './AfrondenButton.scss';
 
 const AfrondenButton = (props: { children: any }) => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.modalText}>Uw melding</Text>
-            <Text style={styles.closeButton}>Afronden</Text>
-            <View style={styles.closeButton}>{props.children}</View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.modalText}>Uw melding</Text>
+      <Text style={styles.closeButton}>Afronden</Text>
+      <View style={styles.closeButton}>{props.children}</View>
+    </View>
+  );
 };
 
 export default AfrondenButton;
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -8,44 +8,39 @@ import { Text, View } from '../components/Themed';
 import AfrondenButton from '../components/AfrondenButton/AfrondenButton';
 
 const TabOneScreen = () => {
-    const [currentStep, setNextStep] = useState(0);
-    const maxSteps = 5;
-    const stepSize = 1 / maxSteps;
+  const [currentStep, setNextStep] = useState(0);
+  const maxSteps = 5;
+  const stepSize = 1 / maxSteps;
 
-    function handleStepNext() {
-        if (currentStep + stepSize <= 1) {
-            setNextStep(currentStep + stepSize);
-        }
+  function handleStepNext() {
+    if (currentStep + stepSize <= 1) {
+      setNextStep(currentStep + stepSize);
     }
+  }
 
-    function handleStepPrevious() {
-        if (currentStep - stepSize >= 0) {
-            setNextStep(currentStep - stepSize);
-        }
+  function handleStepPrevious() {
+    if (currentStep - stepSize >= 0) {
+      setNextStep(currentStep - stepSize);
     }
+  }
 
-    return (
-        <>
-            <Header />
+  return (
+    <>
+      <Header />
 
-            <AfrondenButton>
-                <Pressable
-                    style={styles.button}
-                    onPress={() => setModalVisible(false)}
-                >
-                    <Text style={[styles.modalText, { fontSize: 20 }]}>
-                        Afronden
-                    </Text>
-                </Pressable>
-            </AfrondenButton>
+      <AfrondenButton>
+        <Pressable style={styles.button} onPress={() => setModalVisible(false)}>
+          <Text style={[styles.modalText, { fontSize: 20 }]}>Afronden</Text>
+        </Pressable>
+      </AfrondenButton>
 
-            <View style={styles.container}>
-                <ProgressBar maxSteps={maxSteps} currentStep={currentStep} />
-                <Button onPress={handleStepNext} title="Next" />
-                <Button onPress={handleStepPrevious} title="Previous" />
-            </View>
-        </>
-    );
+      <View style={styles.container}>
+        <ProgressBar maxSteps={maxSteps} currentStep={currentStep} />
+        <Button onPress={handleStepNext} title="Next" />
+        <Button onPress={handleStepPrevious} title="Previous" />
+      </View>
+    </>
+  );
 };
 
 export default TabOneScreen;
