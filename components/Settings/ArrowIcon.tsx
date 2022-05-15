601920127c9cd565cb8f469e3d4d55d40b73b2c5
Koen Janssen <mail@koenjanssen.dev>
2022-05-15 14:13:53 +0200

    Created settings page

---
+++ components/Settings/ArrowIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text } from 'react-native';
+import styles from './Settings.scss';
+
+const ArrowIcon = (props) => {
+  return <Text style={styles.arrowIcon}>{'>'}</Text>;
+};
+
+export default ArrowIcon;
--- components/Settings/Settings.tsx
+++ components/Settings/Settings.tsx
@@ -1,20 +1,52 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Switch, TouchableOpacity } from 'react-native';
+
+import Divider from '../Divider';
+import ArrowIcon from './ArrowIcon';
 import styles from './Settings.scss';
 
 const Settings = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
   const logOut = () => {
-    console.log('logout');
+    // pass
   };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Instellingen</Text>
+
       <Text style={styles.header}>Berichtgeving</Text>
-      <Text style={styles.text}>Push-notificaties</Text>
-      <Text style={styles.header}>Veelgestelde vragen</Text>
+
+      <View style={{ flexDirection: `row`, paddingBottom: 15 }}>
+        <Text style={styles.text}>Push-notificaties</Text>
+        <Switch
+          trackColor={{ false: '#f75a5a', true: '#83DF55' }}
+          thumbColor={isEnabled ? '#52BB1D' : '#C31515'}
+          ios_backgroundColor="#f75a5a"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+          style={{ marginVertical: -5, marginLeft: 15 }}
+        />
+      </View>
+
+      <Divider colorDrawer="black" heightDrawer={0.5} widthDrawer={2} />
+
+      <Text style={styles.header}>
+        Veelgestelde vragen <ArrowIcon />
+      </Text>
+
+      <Divider style={styles.divider} colorDrawer="black" heightDrawer={0.5} widthDrawer={2} />
+
       <Text style={styles.header}>Uw account</Text>
-      <Text>Log in met office account. Voor meer vragen neem contact op met Goevaers Bouwonderneming B.V.</Text>
-      <Button title="Uitloggen" onPress={logOut} />
+      <Text style={styles.text}>
+        Log in met office account. Voor meer vragen neem contact op met Goevaers Bouwonderneming B.V.
+      </Text>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={logOut} style={[styles.logoutButton]}>
+          <Text style={styles.textButton}>Uitloggen</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
