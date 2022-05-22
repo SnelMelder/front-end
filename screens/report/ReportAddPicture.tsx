f410e077180fbc28dc81b3de7d7eb9b040ee9bea
Tara Willink <tarawillink@gmail.com>
2022-05-22 10:06:11 +0200

    Auto stash before merge of "feature/add-photo-screen" and "origin/development"

--- screens/report/ReportAddPicture.tsx
+++ screens/report/ReportAddPicture.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import styles from '../shared.scss';
 
 import { Text, View } from '../../components/Themed';
+import AddPhoto from '../../components/AddPhoto/AddPhoto';
 
 const ReportAddPicture = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Voeg een foto toe</Text>
+
+      <AddPhoto/>
+      
     </View>
   );
 };
