2f26cdfa3575af4b37f56b1297b1de127daa9266
Koen Janssen <mail@koenjanssen.dev>
2022-05-30 11:52:30 +0200

    Fixed header and added Inputbox to other screens.

--- screens/report/ReportAdditionalInformation.tsx
+++ screens/report/ReportAdditionalInformation.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from '../shared.scss';
 
 import { Text, View } from '../../components/Themed';
+import InputBox from '../../components/InputBox/InputBox';
 
 const ReportAdditionalInformation = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Aanvullende informatie</Text>
+      <InputBox placeholder="Typ hier uw aanvullende informatie" />
     </View>
   );
 };
--- screens/report/ReportPersonInvolved.tsx
+++ screens/report/ReportPersonInvolved.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from '../shared.scss';
 
 import { Text, View } from '../../components/Themed';
+import InputBox from '../../components/InputBox/InputBox';
 
 const ReportPersonInvolved = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Naam betrokkene</Text>
+      <InputBox placeholder="Typ hier naam betrokkene" />
     </View>
   );
 };
