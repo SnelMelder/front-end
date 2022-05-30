7b52d364fb4f8627911ad4bbc00a52fb5bbbd5e2
Tara Willink <tarawillink@gmail.com>
2022-05-30 12:26:32 +0200

    Add photo page without functionality

--- screens/report/ReportAddPicture.tsx
+++ screens/report/ReportAddPicture.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
-import styles from '../shared.scss';
+import React, {useCallback, useEffect, useState} from 'react';
 
-import { Text, View } from '../../components/Themed';
-import AddPhoto from '../../components/AddPhoto/AddPhoto';
+import { View, Text, Image, Pressable, Platform } from 'react-native';
+import styles from './ReportAddPicture.scss';
+import * as ImagePicker from 'react-native-image-picker';
 
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faPlus, faRefresh, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { useMediaLibraryPermissions } from 'expo-image-picker';
+import * as ExpoImagePicker from 'expo-image-picker';
 const ReportAddPicture = () => {
+    const openGallery = async() => {
+    }
+    function Restart() {
+    }
+    function chooseMainImage() {
+    }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Voeg een foto toe</Text>
+        <View style={styles.mainImageContainer}>
+            <Image 
+                source={require('../../assets/images/goevaers_logo.png')}
+                style={styles.mainImage}
+            />
+            <Pressable onPress={DeleteImage} style={styles.deleteButton}>
+                <FontAwesomeIcon icon={faTrashCan} size={32}></FontAwesomeIcon>
+            </Pressable>
+        </View> 
 
-      <AddPhoto/>
+        <View style={styles.smallPressables}>
+            <Pressable onPress={openGallery} style={styles.addButton}>
+                <FontAwesomeIcon icon={faPlus} size={32}></FontAwesomeIcon>
+            </Pressable>
+            <Pressable onPress={Restart} style={styles.restartButton}>
+                <FontAwesomeIcon icon={faRefresh} size={32}></FontAwesomeIcon>
+            </Pressable>
+        </View>
       
     </View>
   );
