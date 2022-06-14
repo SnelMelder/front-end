08b12565d5071cff9def11eb7ebe903f3987bae2
Tara Willink <tarawillink@gmail.com>
2022-06-14 11:59:49 +0200

    Added Add photo screen working

--- screens/report/ReportAddPicture.tsx
+++ screens/report/ReportAddPicture.tsx
@@ -1,80 +1,136 @@
-import React, {useCallback, useEffect, useState} from 'react';
-
-import ButtonInformation from '../../components/ButtonInformation/ButtonInformation';
-import { View, Text, Image, Pressable, Platform, Modal } from 'react-native';
+import React, { useEffect, useState} from 'react';
+import { View, Text, Image, Pressable, Platform, Modal, FlatList } from 'react-native';
 import styles from './ReportAddPicture.scss';
-import ImagePickerCom from '../../components/ImagePicker/ImagePicker'
-//import * as ImagePicker from 'react-native-image-picker';
+import sharedStyles from '../shared.scss';
+import ButtonInformation from '../../components/ButtonInformation/ButtonInformation';
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faPlus, faRefresh, faTrashCan } from '@fortawesome/free-solid-svg-icons';
-import { useMediaLibraryPermissions } from 'expo-image-picker';
 import * as ImagePicker from 'expo-image-picker';
 
-var listImages = [];
-
 const ReportAddPicture = () => {
-    
-    const [selectedImage, setSelectedImage] = useState(null);
-    const [pickerResponse, setPickerResponse] = useState<ImagePicker.ImageInfo>();
-    const [modalVisible, setModalVisible] = useState(false);
-
-    function DeleteImage() {
-
-    }
-
-    useEffect(() => {
-      (async () => {
-        if (Platform.OS !== 'web') {
-          const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-          if (status !== 'granted') {
-            alert('Sorry, we need camera roll permissions to choose a photo.');
-          }
+  const [selectedImage, setSelectedImage] = useState<{id: number, image: ImagePicker.ImageInfo} | undefined>();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [list, setList] = useState<{id: number, image: ImagePicker.ImageInfo}[]>([]);
+  
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to choose a photo.');
         }
-      })();
-    }, []);
-
-    const openGallery = async() => {
-      //const options: ImagePicker.ImageLibraryOptions;
-      const status = ImagePicker.requestMediaLibraryPermissionsAsync();
-      console.log(status);
-      const image = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: true
-      });
-
-      if (!image.cancelled) {
-        console.log(image);
-        setPickerResponse(image);
-        setModalVisible(false);
       }
-      
+    })();
+  }, []);
+
+  
+  const openGallery = async() => {
+    const status = ImagePicker.requestMediaLibraryPermissionsAsync();
+    console.log(status);
+    const image = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      allowsMultipleSelection: false,
+      base64: true
+    });
+
+    if (!image.cancelled) {
+      list.push({id: list.length + 1, image: image});
+      setSelectedImage({id: list.length + 1, image: image});
+      setModalVisible(false);
     }
+  }
 
-    const takePicture = async() => {
-      const status = ImagePicker.requestCameraPermissionsAsync();
-      const image = await ImagePicker.launchCameraAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: true
-      })
+  const takePicture = async() => {
+    const status = ImagePicker.requestCameraPermissionsAsync();
+    const image = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      base64: true
+    })
 
-      if (!image.cancelled) {
-        console.log(image);
-        setPickerResponse(image);
-        setModalVisible(false);
+    if (!image.cancelled) {
+      list.push({id: list.length + 1, image: image});
+      setSelectedImage({id: list.length + 1, image: image});
+      setModalVisible(false);
+    }
+  }
+
+  function DeleteImage() {
+    if(typeof selectedImage?.id === "number"){
+      let int = (selectedImage?.id-1);
+      list.splice(int, 1);
+      let imageBefore = list.at(int-1);
+      if(typeof imageBefore !== "undefined"){
+        setSelectedImage({id: imageBefore.id, image: imageBefore.image});
       }
     }
+  }
 
-    function Restart() {
+  function Restart() {
+    list.splice(0, list.length);
+    setSelectedImage(undefined);
+  }
 
+  function chooseMainImage(id: number) {
+    const item = list.at(id);
+    if(typeof item?.id === "number" && typeof item.image){
+      setSelectedImage({id: item.id, image: item.image});
     }
-
-    function chooseMainImage() {
-      
-    }
+  }
 
   return (
     <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={sharedStyles.title}>Voeg een photo toe</Text>
+        <ButtonInformation/>
+      </View>
+      <View style={styles.mainImageContainer}>
+        {selectedImage
+          ? <Image
+              source={{uri: `data:image/jpg;base64,${selectedImage.image.base64}`}}
+              style={styles.mainImage}
+            /> 
+          : null}
+        {selectedImage
+          ?<Pressable onPress={() => DeleteImage()} style={styles.deleteButton}>
+              <FontAwesomeIcon icon={faTrashCan} size={32}></FontAwesomeIcon>
+          </Pressable> 
+        : null}
+      </View> 
+      
+      <View style={styles.smallPressablesContainer}>
+        <View>
+          {selectedImage
+          ? <FlatList
+            data={list}
+            renderItem={({item, index}) =>
+              <Pressable onPress={() => chooseMainImage(index)} style={styles.smallImagePressable}>
+                <Image
+                  source={{uri: `data:image/jpg;base64,${item.image.base64}`}}
+                  style={styles.smallImage}
+                />
+              </Pressable>
+            }
+            horizontal={true}
+            contentContainerStyle={styles.flatListContainer}
+          />
+          : null}
+        </View>
+
+        <Pressable onPress={() => setModalVisible(true)} style={styles.addButton}>
+          <FontAwesomeIcon icon={faPlus} size={35} style={styles.icons}></FontAwesomeIcon>
+        </Pressable>
+        {list.length
+          ? <Pressable onPress={() => Restart()} style={styles.restartButton}>
+            <FontAwesomeIcon icon={faRefresh} size={35}></FontAwesomeIcon>
+          </Pressable>
+        : null}
+        
+      </View>
+
+      {/* Pop up modal when adding a new photo */}
       <Modal
         animationType='slide'
         transparent={true}
@@ -85,47 +141,77 @@ const ReportAddPicture = () => {
       >
         <View style={styles.popupContainer}>
           <View style={styles.modal}>
-            {/* <ImagePickerCom></ImagePickerCom> */}
-            <Pressable onPress={takePicture} style={styles.addButton}>
-              <Text>Take an image</Text>
+            <Pressable onPress={takePicture} style={styles.modalButton}>
+              <Text style={styles.modalText}>Take an image</Text>
             </Pressable>
-            <Pressable onPress={openGallery} style={styles.addButton}>
-              <Text>Choose an Image</Text>
+            <Pressable onPress={openGallery} style={styles.modalButton}>
+              <Text style={styles.modalText}>Choose an Image</Text>
             </Pressable>
-            <Pressable onPress={() => setModalVisible(false)} style={styles.addButton}>
-              <Text>Cancel</Text>
+            <Pressable onPress={() => setModalVisible(false)} style={styles.modalButtonCancel}>
+              <Text style={styles.modalText}>Cancel</Text>
             </Pressable>
           </View>
         </View>
       </Modal>
-
-      <Text style={styles.title}>Voeg een foto toe</Text>
-          {/* <Image 
-                source={{uri: pickerResponse?.uri}}
-                style={styles.mainImage}
-            /> */}
-        <View style={styles.mainImageContainer}>
-            <Image
-                source={{uri: pickerResponse?.uri}}
-                style={styles.mainImage}
-            />
-            <Pressable onPress={DeleteImage} style={styles.deleteButton}>
-                <FontAwesomeIcon icon={faTrashCan} size={32}></FontAwesomeIcon>
-            </Pressable>
-        </View> 
-            
-        <View style={styles.smallPressables}>
-            {/* List of small images? with pressable to select them*/}
-            <Pressable onPress={() => setModalVisible(true)} style={styles.addButton}>
-                <FontAwesomeIcon icon={faPlus} size={32}></FontAwesomeIcon>
-            </Pressable>
-            <Pressable onPress={Restart} style={styles.restartButton}>
-                <FontAwesomeIcon icon={faRefresh} size={32}></FontAwesomeIcon>
-            </Pressable>
-        </View>
             
     </View>
   );
 };
 
 export default ReportAddPicture;
+
+{/* List of small images? with pressable to select them */}
+        {/* {list.forEach(element => {
+          <Pressable onPress={() => chooseMainImage(0)} style={styles.smallImagePressable}>
+          <Image
+            source={{uri: `data:image/jpg;base64,${element.base64}`}}
+            style={styles.smallImage}
+            // styles={styles.mainImage}
+          />
+          </Pressable>
+        })} */}
+        {/* {list.map(() => {
+          <Text>index: {list[index]}, image: {list[index].base64}</Text>
+        })} */}
+
+        
+        {/* {list
+          ? <Pressable onPress={() => chooseMainImage(0)} style={styles.smallImagePressable}>
+            ? <Image
+              source={{uri: `data:image/jpg;base64,${list.at(0)?.base64}`}}
+              style={{width: 200, height: 200}}
+              // styles={styles.mainImage}
+            />
+          </Pressable>
+        : null} */}
+
+        {/* <Pressable onPress={() => chooseMainImage(0)} style={styles.smallImagePressable}>
+          <Image
+            source={require('../../assets/images/bulldozer.jpg')}
+            style={styles.smallImage}
+          />
+        </Pressable>
+        <Pressable onPress={() => chooseMainImage(0)} style={styles.smallImagePressable}>
+          <Image
+            source={require('../../assets/images/bulldozer.jpg')}
+            style={styles.smallImage}
+          />
+        </Pressable>
+        <Pressable onPress={() => chooseMainImage(0)} style={styles.smallImagePressable}>
+          <Image
+            source={require('../../assets/images/bulldozer.jpg')}
+            style={styles.smallImage}
+          />
+        </Pressable>
+        <Pressable onPress={() => chooseMainImage(0)} style={styles.smallImagePressable}>
+          <Image
+            source={require('../../assets/images/bulldozer.jpg')}
+            style={styles.smallImage}
+          />
+        </Pressable>
+        <Pressable onPress={() => chooseMainImage(0)} style={styles.smallImagePressable}>
+          <Image
+            source={require('../../assets/images/bulldozer.jpg')}
+            style={styles.smallImage}
+          />
+        </Pressable> */}
