9d2ccd182810262c2968f1bf8c1ab990d9c5b917
Jordy Arntz <hello@jordyarntz.com>
2022-06-20 20:20:25 +0200

    Fix build errors

--- screens/report/ReportAddPicture.tsx
+++ screens/report/ReportAddPicture.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Image, Pressable, Platform, Modal, FlatList } from 'react-native';
 import styles from './ReportAddPicture.scss';
 import sharedStyles from '../shared.scss';
@@ -9,10 +9,10 @@ import { faPlus, faRefresh, faTrashCan } from '@fortawesome/free-solid-svg-icons
 import * as ImagePicker from 'expo-image-picker';
 
 const ReportAddPicture = () => {
-  const [selectedImage, setSelectedImage] = useState<{id: number, image: ImagePicker.ImageInfo} | undefined>();
+  const [selectedImage, setSelectedImage] = useState<{ id: number; image: ImagePicker.ImageInfo } | undefined>();
   const [modalVisible, setModalVisible] = useState(false);
-  const [list, setList] = useState<{id: number, image: ImagePicker.ImageInfo}[]>([]);
-  
+  const [list, setList] = useState<{ id: number; image: ImagePicker.ImageInfo }[]>([]);
+
   useEffect(() => {
     (async () => {
       if (Platform.OS !== 'web') {
@@ -24,46 +24,45 @@ const ReportAddPicture = () => {
     })();
   }, []);
 
-  
-  const openGallery = async() => {
+  const openGallery = async () => {
     const status = ImagePicker.requestMediaLibraryPermissionsAsync();
     console.log(status);
     const image = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       allowsMultipleSelection: false,
-      base64: true
+      base64: true,
     });
 
     if (!image.cancelled) {
-      list.push({id: list.length + 1, image: image});
-      setSelectedImage({id: list.length + 1, image: image});
+      list.push({ id: list.length + 1, image: image });
+      setSelectedImage({ id: list.length + 1, image: image });
       setModalVisible(false);
     }
-  }
+  };
 
-  const takePicture = async() => {
+  const takePicture = async () => {
     const status = ImagePicker.requestCameraPermissionsAsync();
     const image = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      base64: true
-    })
+      base64: true,
+    });
 
     if (!image.cancelled) {
-      list.push({id: list.length + 1, image: image});
-      setSelectedImage({id: list.length + 1, image: image});
+      list.push({ id: list.length + 1, image: image });
+      setSelectedImage({ id: list.length + 1, image: image });
       setModalVisible(false);
     }
-  }
+  };
 
   function DeleteImage() {
-    if(typeof selectedImage?.id === "number"){
-      let int = (selectedImage?.id-1);
+    if (typeof selectedImage?.id === 'number') {
+      let int = selectedImage?.id - 1;
       list.splice(int, 1);
-      let imageBefore = list.at(int-1);
-      if(typeof imageBefore !== "undefined"){
-        setSelectedImage({id: imageBefore.id, image: imageBefore.image});
+      let imageBefore = list.at(int - 1);
+      if (typeof imageBefore !== 'undefined') {
+        setSelectedImage({ id: imageBefore.id, image: imageBefore.image });
       }
     }
   }
@@ -75,8 +74,8 @@ const ReportAddPicture = () => {
 
   function chooseMainImage(id: number) {
     const item = list.at(id);
-    if(typeof item?.id === "number" && typeof item.image){
-      setSelectedImage({id: item.id, image: item.image});
+    if (typeof item?.id === 'number' && typeof item.image) {
+      setSelectedImage({ id: item.id, image: item.image });
     }
   }
 
@@ -87,52 +86,45 @@ const ReportAddPicture = () => {
         <ButtonInformation title="Voeg een foto toe" text="De foto waar u uw probleem heeft gevonden." />
       </View>
       <View style={styles.mainImageContainer}>
-        {selectedImage
-          ? <Image
-              source={{uri: `data:image/jpg;base64,${selectedImage.image.base64}`}}
-              style={styles.mainImage}
-            /> 
-          : null}
-        {selectedImage
-          ?<Pressable onPress={() => DeleteImage()} style={styles.deleteButton}>
-              <FontAwesomeIcon icon={faTrashCan} size={32}></FontAwesomeIcon>
-          </Pressable> 
-        : null}
-      </View> 
-      
+        {selectedImage ? (
+          <Image source={{ uri: `data:image/jpg;base64,${selectedImage.image.base64}` }} style={styles.mainImage} />
+        ) : null}
+        {selectedImage ? (
+          <Pressable onPress={() => DeleteImage()} style={styles.deleteButton}>
+            <FontAwesomeIcon icon={faTrashCan} size={32}></FontAwesomeIcon>
+          </Pressable>
+        ) : null}
+      </View>
+
       <View style={styles.smallPressablesContainer}>
         <View>
-          {selectedImage
-          ? <FlatList
-            data={list}
-            renderItem={({item, index}) =>
-              <Pressable onPress={() => chooseMainImage(index)} style={styles.smallImagePressable}>
-                <Image
-                  source={{uri: `data:image/jpg;base64,${item.image.base64}`}}
-                  style={styles.smallImage}
-                />
-              </Pressable>
-            }
-            horizontal={true}
-            contentContainerStyle={styles.flatListContainer}
-          />
-          : null}
+          {selectedImage ? (
+            <FlatList
+              data={list}
+              renderItem={({ item, index }) => (
+                <Pressable onPress={() => chooseMainImage(index)} style={styles.smallImagePressable}>
+                  <Image source={{ uri: `data:image/jpg;base64,${item.image.base64}` }} style={styles.smallImage} />
+                </Pressable>
+              )}
+              horizontal={true}
+              contentContainerStyle={styles.flatListContainer}
+            />
+          ) : null}
         </View>
 
         <Pressable onPress={() => setModalVisible(true)} style={styles.addButton}>
           <FontAwesomeIcon icon={faPlus} size={35} style={styles.icons}></FontAwesomeIcon>
         </Pressable>
-        {list.length
-          ? <Pressable onPress={() => Restart()} style={styles.restartButton}>
+        {list.length ? (
+          <Pressable onPress={() => Restart()} style={styles.restartButton}>
             <FontAwesomeIcon icon={faRefresh} size={35}></FontAwesomeIcon>
           </Pressable>
-        : null}
-        
+        ) : null}
       </View>
 
       {/* Pop up modal when adding a new photo */}
       <Modal
-        animationType='slide'
+        animationType="slide"
         transparent={true}
         visible={modalVisible}
         onRequestClose={() => {
@@ -153,7 +145,6 @@ const ReportAddPicture = () => {
           </View>
         </View>
       </Modal>
-            
     </View>
   );
 };
