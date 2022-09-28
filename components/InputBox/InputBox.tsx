c016414fca6db53a6e01c8ff32f1d887b384ab84
jardvanroest <jardvanroest31@gmail.com>
2022-09-28 11:50:06 +0200

    Fix code style issues

--- components/InputBox/InputBox.tsx
+++ components/InputBox/InputBox.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { TextInput } from 'react-native';
 import styles from './InputBox.scss';
 
-const InputBox = props => (
+const InputBox = (props) => (
   <TextInput
     {...props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
     style={styles.inputBox}
