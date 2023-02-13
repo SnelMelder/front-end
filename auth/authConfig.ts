6b8fb70aff88f31a9e63d71dbb9e75b8e6cc33c3
jardvanroest <jardvanroest31@gmail.com>
2023-02-13 13:35:27 +0100

    Remove authConfig from gitignore

---
+++ auth/authConfig.ts
@@ -0,0 +1,13 @@
+import { makeRedirectUri } from 'expo-auth-session';
+
+const TENANT_ID = 'b0ad87a1-0f53-4ca9-a2f2-a0e1b926e061';
+
+export const redirectUri = makeRedirectUri({ useProxy: true });
+export const clientId = 'd9b7bd67-1432-4657-9329-d94b7b35e50e';
+export const discovery = {
+  authorizationEndpoint: `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/authorize`,
+  tokenEndpoint: `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/token`,
+  endSessionEndpoint: `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/logout?client_id=${clientId}`,
+};
+
+export const scopes = ['api://d9b7bd67-1432-4657-9329-d94b7b35e50e/SMBE', 'offline_access'];
