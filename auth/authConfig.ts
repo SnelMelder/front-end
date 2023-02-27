7a81b32ba0050239657249d3a8cc8c1dea07bd42
jardvanroest <jardvanroest31@gmail.com>
2023-02-27 16:43:04 +0100

    Create redirect uri from scheme

--- auth/authConfig.ts
+++ auth/authConfig.ts
@@ -1,8 +1,11 @@
 import { makeRedirectUri } from 'expo-auth-session';
+import Constants from 'expo-constants';
 
 const TENANT_ID = 'b0ad87a1-0f53-4ca9-a2f2-a0e1b926e061';
 
-export const redirectUri = makeRedirectUri({ useProxy: true });
+export const redirectUri = makeRedirectUri({
+  scheme: Constants.manifest?.scheme,
+});
 export const clientId = 'd9b7bd67-1432-4657-9329-d94b7b35e50e';
 export const discovery = {
   authorizationEndpoint: `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/authorize`,
