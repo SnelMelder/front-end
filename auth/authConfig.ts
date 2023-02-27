6a87761438df1049a6a80e25eda90555b757e9b6
jardvanroest <jardvanroest31@gmail.com>
2023-02-27 19:06:30 +0100

    Removed proxy for authentication

--- auth/authConfig.ts
+++ auth/authConfig.ts
@@ -5,6 +5,7 @@ const TENANT_ID = 'b0ad87a1-0f53-4ca9-a2f2-a0e1b926e061';
 
 export const redirectUri = makeRedirectUri({
   scheme: Constants.manifest?.scheme,
+  path: 'auth',
 });
 export const clientId = 'd9b7bd67-1432-4657-9329-d94b7b35e50e';
 export const discovery = {
