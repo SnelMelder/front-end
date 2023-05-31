e0261e2cf9adda5baf9035811c9a28db4716dd05
jardvanroest <jardvanroest31@gmail.com>
2023-05-31 14:52:44 +0200

    Add keyboard avoiding view

--- components/forms/FormQuestion.tsx
+++ components/forms/FormQuestion.tsx
@@ -1,3 +1,13 @@
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Platform,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  View,
+  TextInput,
+  Keyboard,
+} from 'react-native';
 import { Text, Button } from 'react-native-paper';
 import InputContainer from './InputContainer';
 import Container from '../ui/Container';
@@ -12,21 +22,45 @@ interface Props {
   children: React.ReactNode;
 }
 
-const FormQuestion = ({ question, explanation, additionalInfo, onNextQuestion, canSkip = false, canSubmit, children }: Props) => {
+const FormQuestion = ({
+  question,
+  explanation,
+  additionalInfo,
+  onNextQuestion,
+  canSkip = false,
+  canSubmit,
+  children,
+}: Props) => {
   const buttonText = canSkip && !canSubmit ? 'Overslaan' : 'Volgende';
   const canGoNext = canSkip || canSubmit;
-
   return (
-    <Container>
-      <Text variant="titleMedium">{question}</Text>
-      <Text variant="bodyMedium">{explanation}</Text>
-      <Text variant="bodySmall">{additionalInfo}</Text>
-      <InputContainer>{children}</InputContainer>
-      <Button mode="contained" onPress={onNextQuestion} disabled={!canGoNext}>
-        {buttonText}
-      </Button>
-    </Container>
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 0}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.keyboardAvoidingView}
+    >
+      <SafeAreaView style={styles.safeAreaView}>
+        <Container>
+          <Text variant="titleMedium">{question}</Text>
+          <Text variant="bodyMedium">{explanation}</Text>
+          <Text variant="bodySmall">{additionalInfo}</Text>
+          <InputContainer>{children}</InputContainer>
+          <Button mode="contained" onPress={onNextQuestion} disabled={!canGoNext}>
+            {buttonText}
+          </Button>
+        </Container>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+  keyboardAvoidingView: {
+    flex: 1,
+  },
+  safeAreaView: {
+    flex: 1,
+  },
+});
 
 export default FormQuestion;
